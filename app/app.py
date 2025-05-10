1f4204740e1322d74d794a582ec6978f640efa39
Pranavtej1404 <113827801+Pranavtej1404@users.noreply.github.com>
2025-05-10 15:38:06 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -29,4 +29,6 @@ def analyze_paste():
     return jsonify({'suggestions': suggestions})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
+    
